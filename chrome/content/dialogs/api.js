e55324d25c465632dbbff75ade74c750570224ed
ilya <ilya@nivc-235.auriga.ru>
2009-06-23 13:40:42 +0400

    WIP

--- chrome/content/dialogs/api.js
+++ chrome/content/dialogs/api.js
@@ -1,43 +1,44 @@
-const accts = [];
+/* Control the 'api keys' window'
+ * vim:ts=4:sw=4:expandtab:cindent
+ */
+
 const chars = [];
 var acct_list, api_id, api_ltd, api_full, char_list;
+const AcctTreeView = Cc['@aragaer.com/acct-tree-view;1'].
+        getService(Ci.nsITreeView);
 
-function apiClose() {
+function close_api() {
     window.close();
 }
 
 function activate_check() {
     var id = api_id.value;
-    var ltd = api_full.value;
+    var ltd = api_ltd.value;
 
     document.getElementById('btn-check').disabled = (id == '' || ltd == '');
 }
 
-function apiOpen() {
+function on_api_load() {
     acct_list = document.getElementById('acct-list');
     char_list = document.getElementById('char-list');
     api_id = document.getElementById('api-id');
     api_ltd = document.getElementById('api-limited');
     api_full = document.getElementById('api-full');
 
-    acct_list.addEventListener('select', onAcctSelect, true);
-    var accts_tmp = get_all_accounts();
-    for (i in accts_tmp) {
-        println(accts_tmp[i].join("|"));
-        accts.push(accts_tmp[i]);
-        var itm = acct_list.appendItem(accts_tmp[i][0]);
-    }
+    acct_list.addEventListener('select', on_acct_select, true);
+
+    acct_list.apiDB = ApiDB;
+    acct_list.view = AcctTreeView; 
 }
 
-function char_list_empty() {
+function empty_char_list() {
     while (char_list.hasChildNodes()) {
         char_list.removeChild(char_list.firstChild);
     }
-    chars.splice(0);
 }
 
-function onAcctSelect(aEvt) {
-    var row = acct_list.selectedIndex;
+function on_acct_select(aEvt) {
+    var row = acct_list.currentIndex;
     println("Acct select "+row);
 
     if (row == -1) {
@@ -49,51 +50,65 @@ function onAcctSelect(aEvt) {
         api_ltd.disabled = true;
         api_full.disabled = true;
         document.getElementById('btn-check').disabled = true;
+        document.getElementById('btn-remove').disabled = true;
         char_list_empty();
         return;
     }
-    
-    api_id.value = accts[row][1];
-    api_ltd.value = accts[row][2];
-    api_full.value = accts[row][3];
+    api_id.value = accts[row].acct_id;
+    api_ltd.value = accts[row].ltd;
+    api_full.value = accts[row].full;
 
-    var chars_tmp = load_characters(api_id.value);
-    for (i in chars_tmp) {
-        println(chars_tmp[i].join("|"));
-        chars.push(chars_tmp[i]);
-        char_list.appendItem(chars_tmp[i][0]);
-    }
+    load_characters(api_id.value).forEach(function (char) {
+        char_list.appendItem(char[0]);
+    });
 
     api_id.disabled = false;
     api_ltd.disabled = false;
     api_full.disabled = false;
+    document.getElementById('btn-remove').disabled = false;
     activate_check();
 }
 
-function apiCheck() {
-    var id = document.getElementById('api-id').value;
-    var ltd = document.getElementById('api-limited').value;
-    println("Checking keys "+id+":"+ltd);
-    var ret = EveApiService.getCharacterList(id, ltd);
+function check_data() {
+    var row = acct_list.currentIndex;
+    if (row == -1)
+        return;
+
+    accts[row].acct_id = document.getElementById('api-id').value;
+    accts[row].ltd = document.getElementById('api-limited').value;
+    accts[row].full = document.getElementById('api-full').value;
+
+    store_keys(accts[row]);
+    
+    var ret = request_char_list(accts[row].id);
     if (!ret) {
         alert(document.getElementById('wrong-creds').value);
         return;
     }
+    empty_char_list();
 
-    char_list_empty()
-
-    for (node = ret.firstChild; node; node = node.nextSibling) {
-        if (!node.hasAttributes())
-            continue; 
-        var li = document.createElement('listitem');
-        li.setAttribute('label', node.getAttribute('name'));
-        char_list.appendChild(li);
-        chars.push([node.getAttribute('name'), node.getAttribute('characterID'), node.getAttribute('corporationID')]);
-    }
-    store_characters(id, chars);
+    ret.forEach(function (char) {
+        char_list.appendItem(char[0]);
+    });
 }
 
-function apiAdd() {
-    accts.push(['','','','']);
-    acct_list.appendItem('123');
+function add_data() {
+    add_empty_account();
+    acct_list.view = AcctTreeView; 
+}
+
+function reload_accts() {
+    accts.splice(0);
+    get_accounts().forEach(function (a) {
+        accts.push(a);
+    });
+    acct_list.view = AcctTreeView;
+}
+
+function remove_data() {
+    var row = acct_list.selectedIndex;
+    if (row == -1)
+        return;
+
+    
 }
--- chrome/content/tools/api.js
+++ chrome/content/tools/api.js
@@ -20,19 +20,82 @@ function init_api_db() {
 }
 
 function do_check_and_create_tables(conn) {
-    
+    try {
+        conn.doSelectQuery("select 1 from accounts;");
+    } catch (e) {
+        conn.executeSimpleSQL("CREATE TABLE accounts " +
+            "(name char, id integer, acct_id integer, ltd char, full char, primary key (id));");
+    }
+    try {
+        conn.doSelectQuery("select 1 from characters;");
+    } catch (e) {
+        conn.executeSimpleSQL("CREATE TABLE characters " +
+            "(name char, id integer, account integer, corporation integer, primary key (id));");
+    }
+    println("ApiDB initialized");
 }
 
-function get_all_accounts() {
-    return ApiDB.doSelectQuery("select * from accounts;");
-}
-
-function store_characters(acct, chars) {
+function store_characters(id, chars) {
     for (i in chars) {
         ApiDB.executeSimpleSQL("replace into characters values ('"+chars[i][0]+"',"+chars[i][1]+","+acct+");");
     }
 }
 
 function load_characters(acct) {
-    return ApiDB.doSelectQuery("select name, id from characters where account="+acct+";");
+    return acct
+        ? ApiDB.doSelectQuery("select name, id from characters where account="+acct+";")
+        : [];
+}
+
+function add_empty_account() {
+    ApiDB.executeSimpleSQL("insert into accounts (name) values ('');");
+}
+
+function get_accounts() {
+    var res = [];
+    ApiDB.doSelectQuery("select name, id, acct_id, ltd, full from accounts;",
+        function (array) {
+            res.push({
+                name: array[0],
+                id  : array[1],
+                acct_id: array[2],
+                ltd : array[3],
+                full: array[4]
+            });
+            println(array[1]);
+            array.splice(0);
+        }
+    );
+    return res;
+}
+
+function update_acct_name(id, name) {
+    // We need some way to escape data...
+    ApiDB.executeSimpleSQL("update accounts set name='"+name+"' where id="+id+";");
+}
+
+function store_keys(acct_data) {
+    ApiDB.executeSimpleSQL("update accounts set acct_id='"+acct_data.acct_id+"', " +
+        "ltd='"+acct_data.ltd+"', full='"+acct_data.full+"' " +
+        "where id="+acct_data.id+";");
+}
+
+function request_char_list(id) {
+    var data = ApiDB.doSelectQuery("select acct_id, ltd from accounts where id='"+id+"';");
+    var list = EveApiService.getCharacterList(data[0], data[1]);
+    if (!list)
+        return;
+
+    var result = [];
+    for (var node = list.firstChild; node; node = node.nextSibling) {
+        if (!node.hasAttributes())
+            continue; 
+        result.push([node.getAttribute('name'),
+            node.getAttribute('characterID'),
+            node.getAttribute('corporationID')
+        ]);
+    }
+
+    store_characters(data[0], result);
+    return result;
 }
