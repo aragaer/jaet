81f13b4dab2a03d21083e00a1d81d6275d43a5dd
Ilya Konovalov <aragaer@gmail.com>
2009-07-26 00:37:58 +0400

    Additional debugging for assets

---
+++ chrome/content/tools/assets.js
@@ -0,0 +1,11 @@
+function loadAssets() {
+    var ch = document.getElementById('character');
+    var chid = EveApi.getCharByName(ch.value);
+
+    if (chid == 0) {
+        alert("No character '"+ch.value+"' found");
+        return;
+    }
+
+    EveApi.get_character_assets(chid);
+}
