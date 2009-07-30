1f4763b94d76ac54ffc68afaf39271a647da4631
Ilya Konovalov <aragaer@gmail.com>
2009-07-30 22:28:57 +0400

    Files for assets debugging

--- chrome/content/tools/assets.js
+++ chrome/content/tools/assets.js
@@ -1,3 +1,29 @@
+const my_assets = [];
+var assetsTreeView = {
+    rowCount : 10000,
+    getCellText : function (row,column) {
+        switch(column.id) {
+        case 'item':
+            return my_assets[row].toString();
+        case 'loc':
+            return '';
+        case 'count':
+            return my_assets[row].quantity;
+        default:
+            return '';
+        }
+    },
+    setTree: function (treebox) { this.treebox = treebox; },
+    isContainer: function (row) { return false; },
+    isSeparator: function (row) { return false; },
+    isSorted: function () { return false; },
+    getLevel: function (row) { return 0; },
+    getImageSrc: function (row,col) { return null; },
+    getRowProperties: function (row,props) {},
+    getCellProperties: function (row,col,props) {},
+    getColumnProperties: function (colid,col,props) {}
+};
+
 function loadAssets() {
     var ch = document.getElementById('character');
     var chid = EveApi.getCharByName(ch.value);
@@ -7,5 +33,14 @@ function loadAssets() {
         return;
     }
 
-    EveApi.get_character_assets(chid);
+    var result = EveApi.getCharacterAssets(chid);
+
+    my_assets.splice(0);
+
+    result.forEach(function (a) {
+        my_assets.push(a);
+    });
+    assetsTreeView.rowCount = my_assets.length;
+    document.getElementById('assets').view = assetsTreeView;
 }
+
